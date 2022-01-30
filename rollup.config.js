e0d91c1383f9f112439b0ce163a62f557e6ffb31
zhoujie <zhoujie16m@163.com>
2022-01-30 21:23:34 +0800

    commit

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,8 @@
 // rollup.config.js
+import resolve from "rollup-plugin-node-resolve";
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
 export default {
   input: "src/index.js",
   output: {
@@ -6,4 +10,16 @@ export default {
     format: "umd",
     name: "ZxmUtils",
   },
+  plugins: [
+    resolve(),
+    babel({
+      exclude: "**/node_modules/**",
+    }),
+    commonjs(),
+    terser({
+      compress: {
+        drop_console: true,
+      },
+    }),
+  ],
 };
